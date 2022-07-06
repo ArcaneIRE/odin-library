7d36a0970e7b2fed14297ea9c2a2793a06b2b9e7
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2022-07-06 20:21:19 +0100

    Add library render function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,19 @@ function addBookToLibrary(title, author, isRead) {
     const newBook = new Book(title, author, isRead);
     library.push(newBook);
 }
+
+function renderLibrary () {
+    let bookList = document.querySelector('#book-list');
+    bookList.innerHTML = '';
+    library.forEach((book, index) => {
+        bookList.innerHTML +=
+            `<div class="book" data-index="${index}">
+                <h3 class="title">${book.title}</h3>
+                <p class="author">${book.author}</p>
+                <label>
+                    <input type="checkbox" ${book.isRead ? ' checked' : ''}>
+                    Read
+                </label>
+            </div>`;
+    });
+}
